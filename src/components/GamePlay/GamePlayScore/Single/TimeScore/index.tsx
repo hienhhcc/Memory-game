811ebf9df9be3c7d6389714dec65ba5911496cc8
vuvinhhien <thachdau16t@gmail.com>
2022-04-27 17:56:41 +0700

    pause timer when game end and reset and start timer when the game restart

--- src/components/GamePlay/GamePlayScore/Single/TimeScore/index.tsx
+++ src/components/GamePlay/GamePlayScore/Single/TimeScore/index.tsx
@@ -4,12 +4,22 @@ import usePlaying from "../../../../../contexts/usePlaying";
 import Score from "../Score";
 
 const TimeScore = () => {
-  const { setPlayerOneTimeTaken } = usePlaying();
-  const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
-    useStopwatch({ autoStart: true });
+  const { setPlayerOneTimeTaken, isGameFinished } = usePlaying();
+  const { seconds, minutes, isRunning, start, pause, reset } = useStopwatch({
+    autoStart: true,
+  });
 
   let displayMinutes = minutes < 10 ? "0" + minutes : minutes;
   let displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+
+  useEffect(() => {
+    if (isGameFinished) {
+      pause();
+    } else if (!isRunning && !isGameFinished) {
+      reset();
+      start();
+    }
+  }, [isGameFinished, pause, reset, isRunning, start]);
 
   useEffect(() => {
     setPlayerOneTimeTaken({ minute: minutes, second: seconds });
